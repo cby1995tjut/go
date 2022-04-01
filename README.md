7ddbc038e661ba1f996c23e56edcb3a7bcc16a34
davon <747664918@qq.com>
2022-04-01 22:35:22 +0800

    Add array demo

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # go
 My first GO project, just for learn
+
+
+问题记录:
+= 是赋值， := 是声明变量并赋值。
+
---
+++ src/main/arrayDemo/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var hens [2]float64
+	hens[0] = 1.0
+	hens[1] = 2.0
+	total := 0.0
+	for i := 0; i < len(hens); i++ {
+		total += hens[i]
+	}
+	fmt.Print(total)
+}
