8143635487855d5bef9e46ce40aac76cbfa3d04f
davon <747664918@qq.com>
2022-04-05 21:48:26 +0800

    panic and recover tester

---
+++ src/main/panicAndRecover/demo01.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+)
+
+func func1() {
+	a := "a"
+	if a == "a" {
+		panic("Nothing need to do")
+	}
+
+	fmt.Print("HAHAHAHA")
+}
+func main() {
+	throwsPanic()
+	fmt.Println("CCCCCC")
+}
+
+// defer + recover 相当与try catch
+func throwsPanic() (b bool) {
+	defer func() {
+		if x := recover(); x != nil {
+			b = true
+		}
+	}()
+	func1()
+	fmt.Println("DIDIDIDIDI") // if f causes panic, it will recover
+	return
+}
+
+func init() {
+	fmt.Println("Im init")
+}
